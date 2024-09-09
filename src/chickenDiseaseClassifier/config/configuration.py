c4a25fa09bedee0b4abfe10b5716a6e843693954
HEMANTH <163557351+hemanth8705@users.noreply.github.com>
2024-09-09 17:59:20 +0530

    dvc added

--- src/chickenDiseaseClassifier/config/configuration.py
+++ src/chickenDiseaseClassifier/config/configuration.py
@@ -1,11 +1,12 @@
 import os
-
+from pathlib import Path
 from chickenDiseaseClassifier.constant import *
 from chickenDiseaseClassifier.utils.common import read_yaml , create_directories
 from chickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig ,
                                                             PrepareBaseModelConfig,
                                                             PrepareCallBackConfiguration,
-                                                            TrainingConfiguration)
+                                                            TrainingConfiguration,
+                                                            EvaluationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -98,3 +99,12 @@ class ConfigurationManager:
         )
 
         return training_config
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model= Path("artifacts/training/model.h5"),
+            training_data= Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
